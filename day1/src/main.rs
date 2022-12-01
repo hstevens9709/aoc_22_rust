35536741f25450c17f7a20b150cf6c34f062cbcb
Harry Stevens <harry.stevens@metaswitch.com>
2022-12-01 17:12:04 +0000

    Day 1 Part 2 Solution.
    Things learned:
    - Vecs
    - Sorting a vec
    - Accessing vec elements
    
    Things I'd like to improve:
    - I'd still like to be able to use function mapping, feels like that would tidy up the nested loops.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,11 @@ use std::fs;
 use std::str::Split;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let file_path = "src/input.txt";
     println!("In file {}", file_path);
 
@@ -21,4 +26,26 @@ fn main() {
         }
     }
     println!("{}", prev_max)
+}
+
+fn part2() {
+    // I could use the same approach as part1 and just keep track of the 3 highest but that seems inelegant. 
+    // Better to sort the list of sums and take the top 3
+    let file_path = "src/input.txt";
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let list = contents.split("\n\n");
+    let mut sums = Vec::<i32>::new();
+    for inner_list in list {
+        let mut sum = 0;
+        for item in inner_list.split("\n") {
+            sum += item.parse::<i32>().unwrap();
+        }
+        sums.push(sum);
+    }
+    sums.sort();
+    println!("{}", sums[sums.len()-1] + sums[sums.len()-2] + sums[sums.len()-3]);
 }
