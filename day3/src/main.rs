481e7b61e9871ccd4c600de339ffe0db7544f2d5
Harry Stevens <harry.stevens@metaswitch.com>
2022-12-12 16:37:16 +0000

    Day 3 Part 2 complete
    Things I learned:
    - Importing a non-std library
    - Chunks and Lines
    - Finally got mapping to work
    - More 'rustful' solution - admittedly copied largely from Simon's solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::{fs, str::Lines};
+use itertools::{Chunk, Itertools};
 
 fn read_file() -> String {
     let file_path = "src/input.txt";
@@ -18,12 +19,25 @@ fn find_common_char(str1: &str, str2: &str) -> char{
     return ' ';
 }
 
+fn find_common_char_from_3_strings(str1: &str, str2: &str, str3: &str) -> char {
+    for c in str1.chars() {
+        if str2.contains(c) && str3.contains(c) { return c; }
+    }
+    return ' ';
+}
+
 fn convert_char_to_num(c: char) -> u32 {
     if c.is_uppercase() {
         return (c as u32) - ('A' as u32) + 27;
     } else {
         return (c as u32) - ('a' as u32) + 1;
     }
+}
+
+fn get_badge_priority(group: Chunk<Lines>) -> u32 {
+    let group_vec = group.collect::<Vec<&str>>();
+    let badge = find_common_char_from_3_strings(group_vec[0], group_vec[1], group_vec[2]);
+    return convert_char_to_num(badge);
 }
 
 fn part1() {
@@ -43,6 +57,13 @@ fn part1() {
     println!("{}", sum);
 }
 
+fn part2() {
+    let input: String = read_file();
+    let result = input.lines().chunks(3).into_iter().map(get_badge_priority).sum::<u32>();
+    println!("{}", result);
+}
+
 fn main() {
     part1();
+    part2();
 }
